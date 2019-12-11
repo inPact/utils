86c826c8126926122229bf2227e032cc4f482e8c
joniba <jbenami1@gmail.com>
2019-12-11 13:37:35 +0200

    lock: await the specified action; separate pending-lock from open-lock traces
    (cherry picked from commit e0eac31baa588b0c8cbc176dd01ac5bc1e910ed3)

--- src/lock.js
+++ src/lock.js
@@ -5,7 +5,8 @@ const randomstring = require('randomstring');
 const logger = require('winston');
 
 const MIN_LOCKS_FOR_DEBUG = process.env.MIN_DOUBLE_CHECK_LOCKS_FOR_DEBUG || 100;
-let counter = 0;
+let pendingCounter = 0;
+let lockedCounter = 0;
 
 async function getOrInitAndSet(getter, initializer, setter) {
     let val = await getter();
@@ -21,10 +22,10 @@ async function getOrInitAndSet(getter, initializer, setter) {
 function debugLocking(lockName) {
     let id = '';
     if (debug.enabled) {
-        counter++;
-        if (counter >= MIN_LOCKS_FOR_DEBUG) {
+        pendingCounter++;
+        if (pendingCounter >= MIN_LOCKS_FOR_DEBUG) {
             id = randomstring.generate({ length: 6, charset: 'alphabetic' });
-            debug(`Double-check-lock ${id}: acquiring lock on ${lockName}. Total pending locks: ${counter}`);
+            debug(`Double-check-lock ${id}: acquiring lock on ${lockName}. Total pending locks: ${pendingCounter}`);
         }
     }
     return id;
@@ -32,9 +33,18 @@ function debugLocking(lockName) {
 
 function debugLockAcquired(id, lockName) {
     if (debug.enabled) {
-        counter--;
+        lockedCounter++;
+        pendingCounter--;
         if (id)
-            debug(`Double-check-lock ${id}: lock acquired on ${lockName}. Total pending locks: ${counter}`);
+            debug(`Double-check-lock ${id}: lock acquired on ${lockName}. Total open/pending locks: ${lockedCounter}/${pendingCounter}`);
+    }
+}
+
+function debugLockReleased(id, lockName) {
+    if (debug.enabled) {
+        lockedCounter--;
+        if (id)
+            debug(`Double-check-lock ${id}: lock released on ${lockName}. Total open locks: ${lockedCounter}`);
     }
 }
 
@@ -52,7 +62,7 @@ module.exports = {
     internal: lock,
 
     get traceCounter() {
-        return counter;
+        return pendingCounter;
     },
 
     async getWithDoubleCheck(getter, namespace, initializer, setter) {
@@ -75,16 +85,19 @@ module.exports = {
     async acquire(namespace, action) {
         let lockId = debugLocking(namespace);
         try {
-            return await lock.acquire(namespace, async () => {
+            let result = await lock.acquire(namespace, async () => {
                 debugLockAcquired(lockId, namespace);
                 return await action();
-            })
+            });
+            debugLockReleased(lockId, namespace);
+            return result;
         } catch (e) {
             if (e.message === 'Too much pending tasks') {
                 logger.error(`async lock exploded on lock "${lockId}" with name "${namespace}". ${e.stack}`);
-                if (debug.enabled)
-                    counter--;
-
+                if (debug.enabled) {
+                    pendingCounter--;
+                    lockedCounter--;
+                }
                 return await action();
             }
 
