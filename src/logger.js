5d1383937bc6f5e6e2e4692240a420a5aa2ba05c
Nati Maskens <nati@tabit.cloud>
2021-10-20 15:30:20 +0300

    [TAB-18398] Add winston transports only if absent

--- src/logger.js
+++ src/logger.js
@@ -4,7 +4,7 @@ const _ = require('lodash');
 const { Console } = winston.transports;
 const { combine, colorize, simple } = winston.format;
 
-if (!winston.transports || !winston.transports.length) {
+if (!winston.default || !winston.default.transports || !winston.default.transports.length) {
     winston.add(new Console({ format: combine( colorize(), simple()) }));
 }
 
